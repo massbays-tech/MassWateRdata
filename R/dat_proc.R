08c3f1183ad316ccb770b7f993ec47f751d7fdfd
fawda123 <mbafs2012@gmail.com>
2023-03-03 08:00:28 -0500

    update ponds, rivers, and streams raw and rdata files for full connecticut and merrimack river watersheds

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -4,25 +4,27 @@ library(here)
 
 tol <- 10
 
-pondsMWR <- st_read(here('data/data-raw/NHDWaterbody_ftype_390-493_vis_101k_noattr.shp')) %>%
+# layers received from BW on 3/2/23 have already been simplified
+
+pondsMWR <- st_read(here('data/data-raw/FullNHDWaterbody_ftype390-493_vis101k_simpl.shp')) %>%
  st_make_valid() %>%
- st_simplify(dTolerance = tol, preserveTopology = TRUE) %>%
+ # st_simplify(dTolerance = tol, preserveTopology = TRUE) %>%
  st_make_valid() %>%
  select(dLevel)
 
 save(pondsMWR, file = here('data/pondsMWR.RData'))
 
-riversMWR <- st_read(here('data/data-raw/NHDArea_noattr.shp')) %>%
+riversMWR <- st_read(here('data/data-raw/FullNHDArea_noattrib_simpl.shp')) %>%
  st_make_valid() %>%
- st_simplify(dTolerance = tol, preserveTopology = TRUE) %>%
+ # st_simplify(dTolerance = tol, preserveTopology = TRUE) %>%
  st_make_valid() %>%
  select(dLevel)
 
 save(riversMWR, file = here('data/riversMWR.RData'))
 
-streamsMWR <- st_read(here('data/data-raw/NHDFlowline_fcode_46006_vis_101k_noattr.shp')) %>%
+streamsMWR <- st_read(here('data/data-raw/FullNHDFlowline_fcode46006_vis101k_simpl.shp')) %>%
  st_zm() %>%
- st_simplify(dTolerance = tol, preserveTopology = TRUE) %>%
+ # st_simplify(dTolerance = tol, preserveTopology = TRUE) %>%
  st_make_valid() %>%
  select(dLevel)
 
