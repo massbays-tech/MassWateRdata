7f3525e9876746170bc2b430d52727c145a4f718
fawda123 <mbafs2012@gmail.com>
2025-12-07 09:33:41 -0500

    create geojson for spatial layers and upload so s3

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,6 +1,14 @@
 library(sf)
 library(dplyr)
 library(here)
+library(geojsonsf)
+library(aws.s3)
+
+Sys.setenv(
+  "AWS_ACCESS_KEY_ID" = Sys.getenv("AWS_ACCESS_KEY_ID"),
+  "AWS_SECRET_ACCESS_KEY" = Sys.getenv("AWS_SECRET_ACCESS_KEY"),
+  "AWS_DEFAULT_REGION" = "us-east-1" 
+)
 
 tol <- 10
 
@@ -13,6 +21,14 @@ pondsMWR <- st_read(here('data/data-raw/FullNHDWaterbody_ftype390-493_vis101k_si
  select(dLevel)
 
 save(pondsMWR, file = here('data/pondsMWR.RData'))
+pondsMWR_geojson_string <- sf_geojson(pondsMWR, atomise = TRUE, simplify = TRUE)
+write(pondsMWR_geojson_string, here("data/pondsMWR.geojson"))
+put_object(
+  file = here("data/pondsMWR.geojson"),
+  object = 'geojson/pondsMWR.geojson',
+  bucket = "masswater-data"
+)
+
 
 riversMWR <- st_read(here('data/data-raw/FullNHDArea_noattrib_simpl.shp')) %>%
  st_make_valid() %>%
@@ -21,6 +37,13 @@ riversMWR <- st_read(here('data/data-raw/FullNHDArea_noattrib_simpl.shp')) %>%
  select(dLevel)
 
 save(riversMWR, file = here('data/riversMWR.RData'))
+riversMWR_geojson_string <- sf_geojson(riversMWR, atomise = TRUE, simplify = TRUE)
+write(riversMWR_geojson_string, here("data/riversMWR.geojson"))
+put_object(
+  file = here("data/riversMWR.geojson"),
+  object = 'geojson/riversMWR.geojson',
+  bucket = "masswater-data"
+)
 
 streamsMWR <- st_read(here('data/data-raw/FullNHDFlowline_fcode46006_vis101k_simpl.shp')) %>%
  st_zm() %>%
@@ -29,6 +52,13 @@ streamsMWR <- st_read(here('data/data-raw/FullNHDFlowline_fcode46006_vis101k_sim
  select(dLevel)
 
 save(streamsMWR, file = here('data/streamsMWR.RData'))
+streamsMWR_geojson_string <- sf_geojson(streamsMWR, atomise = TRUE, simplify = TRUE)
+write(streamsMWR_geojson_string, here("data/streamsMWR.geojson"))
+put_object(
+  file = here("data/streamsMWR.geojson"),
+  object = 'geojson/streamsMWR.geojson',
+  bucket = "masswater-data"
+)
 
 sudburyMWR <- st_read(here('data/data-raw/Sudbury_watershed_singleboundary.shp')) %>%
  st_zm()
